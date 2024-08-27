33aeb524de2a4afde09f1990a55bcdc1a02e30af
arthurpnuness <arthurpessoanunes@gmail.com>
2024-08-27 19:54:20 -0300

    up

--- atv3.py
+++ atv3.py
@@ -6,7 +6,7 @@ raio = float(input("Digite o raio do círculo: "))
 #Calculo
 pi = 3.14159 # Valor aproximado de (pi)
 
-area = pi * raio ** 2 # Calcula a área usando a fórmula - OUtra observação válida é os dois asteriscos, eles elevam o valor do raio ao quadrado
+area = pi * raio ** 2 # Calcula a área usando a fórmula - OUtra observação válida é os dois asteriscos(**), eles elevam o valor do raio ao quadrado
 
 # Exibe o resultado
 print('A área do círculo com raio {} é: {}'.format(raio, area)) 
